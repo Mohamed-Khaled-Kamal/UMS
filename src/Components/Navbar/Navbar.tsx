2c83dac440117dafc009841f95fe6cb7518923ca
Mohamed-Khaled-Kamal <mo7med.khaled22@gmail.com>
2024-12-23 15:52:47 +0200

    last update

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,13 +7,13 @@ import { AuthContext } from '../Context/AuthContext';
 
 export default function NavBar() {
 
-  let {userData} = useContext(AuthContext) 
+  let {userData}:any = useContext(AuthContext) 
 
   return (
     <>
     <Navbar expand="lg" className="bg-body-tertiary sticky-top">
       <Container>
-        <Navbar.Brand href="#home" className='text-warning fs-3'>UMS</Navbar.Brand>
+        <Navbar.Brand  href="#home" className='text-warning fs-3'>UMS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
